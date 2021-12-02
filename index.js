77af2b34e11b672c3bdea1fe6729c52d583dd914
unknown <jnip2@my.bcit.ca>
2021-12-01 16:39:44 -0800

    tuesday 4pm

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get("/people/:id", (req, res) => {
-//   res.render("people");
-//   //const id = somehow need to get the id from the url
-//   //fs.readfile("database.json", "utf8")
-//   //  .then((content) => JSON.parse(content).users)
-//   //need to loop through users and check if id matches
-//   //  .then(users => users.find((user) => user.id = id)) //find is HoF
-//   //  .then((foundUser) => res.render("homepage", { user: foundUser }))
-//   // then remove hardcoded information in HTML with <%= user.name %>
-
-//   //install EJS language support extension
-
-  
-// });
-
 app.get("/", (req, res) => {
   res.render("createcard");
 });
@@ -48,7 +33,6 @@ app.post("/create", (req, res) => {
 
 app.get("/people/:id", (req, res) => {
   const userID = req.params.id;
-  // console.log(userID)
 
   fs.readFile(`${__dirname}/database.json`, "utf-8")
     .then((data) => JSON.parse(data).users)
